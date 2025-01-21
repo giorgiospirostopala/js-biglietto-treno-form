86e2e561e8eedeb9023ea50c55fdf64fb00d870f
giorgiospirostopala <gio.topala@gmail.com>
2025-01-22 00:12:49 +0100

    add: validazione del browser

--- script.js
+++ script.js
@@ -9,11 +9,6 @@
 //-         - va applicato uno sconto del 40% per gli over 65.
 
 
-//?     MILESTONE 2:
-// Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo.
-
-// Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo). Questo richiederà un minimo di ricerca.
-
 //?     MILESTONE 3:
 // Ora che la logica è funzionante in pagina, possiamo andare a dedicarci allo stile, raffinando la parte di HTML e CSS in modo da renderla esteticamente gradevole.
 
@@ -54,22 +49,24 @@ prezzoFinale = prezzoFinale.toFixed(2);
 // Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente due input e un bottone (non stilizzati), realizziamo le specifiche scritte sopra. La risposta finale (o output) sarà anch’essa da scrivere in console.
 
 // vado a pescarmi tutti gli elementi
-const kmInseriti = document.getElementById("km");
-const etaInserita = document.getElementById("eta");
-const bottoneGenera = document.getElementById("btnGenera");
-const bottoneAnnulla = document.getElementById("btnAnnulla");
+const nome = document.getElementById("nome");
+const km = document.getElementById("km");
+const eta = document.getElementById("eta");
+const btnGenera = document.getElementById("btnGenera");
+const btnAnnulla = document.getElementById("btnAnnulla");
 
-// creo l'evento al click del bottone genera, recuperando i dati inseriti 
-bottoneGenera.addEventListener("click", function() {
-    const chilometri = parseFloat(kmInseriti.value);
-    const etaUtente = parseInt(etaInserita.value);
+// creo l'evento al click del bottone genera, recuperando i dati inseriti con .value
+btnGenera.addEventListener("click", function() {
+    const chilometri = parseFloat(km.value);
+    const etaUtente = parseInt(eta.value);
+    const nomeUtente = nome.value;
 
-    //// COSA SO
+    // riprendo i dati che conosco e che devo dichiarare
     const costoBiglAlKm = 0.21;
     const scontoMinorenni = 0.8;
     const scontoAnziani = 0.6; 
 
-    //// CALCOLO IL PREZZO
+    // rimanendo all'interno della funzione di callback, calcolo il prezzo
     let prezzoStandard = chilometri * costoBiglAlKm;
     let prezzoFinale;
 
@@ -83,9 +80,21 @@ bottoneGenera.addEventListener("click", function() {
         prezzoFinale = prezzoStandard;
     };
 
-    //// MOSTRO SOLO DUE DECIMALI
+    // mostro solo due decimali e stampo in console il prezzo finale
     prezzoFinale = prezzoFinale.toFixed(2);
     console.log(`Il prezzo finale del biglietto è: € ${prezzoFinale}`);
-    
 });
 
+// infine creo l'evento al click del bottone annulla, resettando i tre input
+btnAnnulla.addEventListener("click", function () {
+    km.value = "";
+    eta.value = "";
+    nome.value = "";
+});
+
+//*_____________________________________________
+
+//?     MILESTONE 2:
+// Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo.
+
+// Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo). Questo richiederà un minimo di ricerca.
