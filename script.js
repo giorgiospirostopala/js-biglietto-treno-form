d9fa2355aeae629f14cfb9b502f03243abfec7f9
giorgiospirostopala <gio.topala@gmail.com>
2025-01-22 22:14:32 +0100

    add: form.checkValidity( utilissimo );

--- script.js
+++ script.js
@@ -44,7 +44,8 @@ prezzoFinale = prezzoFinale.toFixed(2);
 //?     MILESTONE 1:
 // Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente due input e un bottone (non stilizzati), realizziamo le specifiche scritte sopra. La risposta finale (o output) sarà anch’essa da scrivere in console.
 
-// vado a pescarmi tutti gli elementi
+// vado a pescarmi tutti gli elementi //! incluso il form per la .checkValidity()
+const form = document.querySelector("form");
 const nome = document.getElementById("nome");
 const km = document.getElementById("km");
 const eta = document.getElementById("eta");
@@ -55,37 +56,44 @@ const tipoOfferta = document.getElementById("tipoOfferta");
 
 // creo l'evento al click del bottone genera, recuperando i dati inseriti con .value
 btnGenera.addEventListener("click", function(event) {
-    event.preventDefault();
+    
+    //* !!! .checkValidity() utilissimo per far convivere html-validation + event.preventDefault() !!!
+    if (form.checkValidity()) {
+        event.preventDefault();
 
-    const chilometri = parseFloat(km.value);
-    const etaUtente = parseInt(eta.value);
-    const nomeUtente = nome.value;
+        const chilometri = parseFloat(km.value);
+        const etaUtente = parseInt(eta.value);
+        const nomeUtente = nome.value;
 
-    // riprendo i dati che conosco e che devo dichiarare
-    const costoBiglAlKm = 0.21;
-    const scontoMinorenni = 0.8;
-    const scontoAnziani = 0.6; 
+        // riprendo i dati che conosco e che devo dichiarare
+        const costoBiglAlKm = 0.21;
+        const scontoMinorenni = 0.8;
+        const scontoAnziani = 0.6; 
 
-    // rimanendo all'interno della funzione di callback, calcolo il prezzo
-    let prezzoStandard = chilometri * costoBiglAlKm;
-    let prezzoFinale;
+        // rimanendo all'interno della funzione di callback, calcolo il prezzo
+        let prezzoStandard = chilometri * costoBiglAlKm;
+        let prezzoFinale;
 
-    if (etaUtente < 18){
-        prezzoFinale = prezzoStandard * scontoMinorenni;
+        if (etaUtente < 18){
+            prezzoFinale = prezzoStandard * scontoMinorenni;
 
-    } else if (etaUtente > 65){
-        prezzoFinale = prezzoStandard * scontoAnziani;
+        } else if (etaUtente > 65){
+            prezzoFinale = prezzoStandard * scontoAnziani;
 
+        } else {
+            prezzoFinale = prezzoStandard;
+        };
+
+        // mostro solo due decimali e stampo in console il prezzo finale
+        prezzoFinale = prezzoFinale.toFixed(2);
+
+        // stampo i dati inseriti e il prezzo finale su pagina
+        bigliettoNome.innerHTML = `${nomeUtente}`;
+        bigliettoPrezzoFinale.innerHTML = `€ ${prezzoFinale}`;
+    
     } else {
-        prezzoFinale = prezzoStandard;
+        // pippo
     };
-
-    // mostro solo due decimali e stampo in console il prezzo finale
-    prezzoFinale = prezzoFinale.toFixed(2);
-
-    // stampo i dati inseriti e il prezzo finale su pagina
-    bigliettoNome.innerHTML = `${nomeUtente}`;
-    bigliettoPrezzoFinale.innerHTML = `€ ${prezzoFinale}`;
 });
 
 
